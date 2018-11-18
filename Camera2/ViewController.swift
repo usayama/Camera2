011865fdb4721b96a73e7530962704e9c87d43ba
y-nakane <y-nakane@sakura.ad.jp>
2018-11-18 18:31:12 +0900

    カメラかフォトライブラリーかを選択できるようにコードを修正

--- Camera2/ViewController.swift
+++ Camera2/ViewController.swift
@@ -19,24 +19,65 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     // カメラを起動するをタップすると実行
     @IBAction func cameraButtonAction(_ sender: UIButton) {
+        // カメラかフォトライブラリーどちらから画像を取得するか選択
+        let alertController = UIAlertController(title: "確認", message: "選択してください", preferredStyle: .actionSheet)
+        
         // カメラが利用可能かチェック
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            print("カメラは利用できます")
-            // (1) UIImagePickerControllerのインスタンスを作成
-            let imagePickerController = UIImagePickerController()
-            // (2) sourceType に camera を設定
-            imagePickerController.sourceType = .camera
-            // (3) delegate設置
-            imagePickerController.delegate = self
-            // (4) モーダルビューで表示
-            present(imagePickerController, animated: true, completion: nil)
-        } else {
-            print("カメラは利用できません")
+            // カメラを使用するための選択肢を定義
+            let cameraAction = UIAlertAction(title: "カメラ", style: .default, handler: {(action: UIAlertAction) in
+                // (1) UIImagePickerControllerのインスタンスを作成
+                let imagePickerController = UIImagePickerController()
+                // (2) sourceTypeにカメラを指定
+                imagePickerController.sourceType = .camera
+                // (3) デリゲートを設置
+                imagePickerController.delegate = self
+                // (4) モーダルビューで表示
+                self.present(imagePickerController, animated: true, completion: nil)
+            })
+            alertController.addAction(cameraAction)
         }
+        
+        // フォトライブラリーが使用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            // フォトライブラリーを使用するための選択肢を定義
+            let photoLibraryAction = UIAlertAction(title: "フォトライブラリー", style: .default, handler: {(action: UIAlertAction) in
+                // (1) UIImagePickerControllerのインスタンスを作成
+                let imagePickerController = UIImagePickerController()
+                // (2) sourceTypeにフォトライブラリーを指定
+                imagePickerController.sourceType = .photoLibrary
+                // (3) デリゲートを設置
+                imagePickerController.delegate = self
+                // (4) モーダルビューで表示
+                self.present(imagePickerController, animated: true, completion: nil)
+            })
+            alertController.addAction(photoLibraryAction)
+        }
+        
+        // キャンセルの選択肢を定義
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        // iPadで落ちてしまう対策
+        alertController.popoverPresentationController?.sourceView = view
+        
+        // 選択肢を画面に表示
+        present(alertController, animated: true, completion: nil)
     }
     
     // SNSに投稿するをタップすると実行
     @IBAction func SNSButtonAction(_ sender: UIButton) {
+        // 表示画像があるときだけアンラップしてシェア画像を取り出し
+        if let shareImage = pictureImage.image {
+            // UIActivityViewControllerに渡す配列を作成
+            let shareItems = [shareImage]
+            // UIActivityViewControllerにシェア画像を渡す
+            let controller = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+            // iPadで落ちてしまう対策
+            controller.popoverPresentationController?.sourceView = view
+            // UIActivityViewControllerを表示
+            present(controller, animated: true, completion: nil)
+        }
     }
     
     // (1) 撮影が終わったときに呼ばれるデリゲートメソッド
