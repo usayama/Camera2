bc457d171c328751f25186103794cc348b252858
y-nakane <y-nakane@sakura.ad.jp>
2018-11-18 12:01:00 +0900

    デリゲートを宣言

--- Camera2/ViewController.swift
+++ Camera2/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
