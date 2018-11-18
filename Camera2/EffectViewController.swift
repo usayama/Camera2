cc454a7dc0f76c50387dc209883773aac42cb003
y-nakane <y-nakane@sakura.ad.jp>
2018-11-18 20:09:35 +0900

    Layout : EffectViewControllerのファイルを作成

---
+++ Camera2/EffectViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EffectViewController.swift
+//  Camera2
+//
+//  Created by Yusuke Nakane on 2018/11/18.
+//  Copyright © 2018 usayama. All rights reserved.
+//
+
+import UIKit
+
+class EffectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
